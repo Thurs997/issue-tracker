e6c6bc8737e99918e6e2ddb4a3865567bcb96f82
Bartek Andrzejczak <ba.andrzejczak@gmail.com>
2014-04-27 13:28:17 +0200

    First controller method

--- src/main/java/pl/edu/pw/elka/pik/issueTracker/MainController.java
+++ src/main/java/pl/edu/pw/elka/pik/issueTracker/MainController.java
@@ -1,8 +1,18 @@
 package pl.edu.pw.elka.pik.issueTracker;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * Created by kokoss on 4/27/14.
  */
 @Controller
 public class MainController {
+
+    @RequestMapping(value="/", method=RequestMethod.GET)
+    public String testSite(){
+        return "HelloWorld";
+    }
+
 }
