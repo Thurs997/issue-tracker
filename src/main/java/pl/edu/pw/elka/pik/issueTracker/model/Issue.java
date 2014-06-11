7df5b4705f7d959b9fbdda9329419557734dcf30
Bartek Andrzejczak <ba.andrzejczak@gmail.com>
2014-06-11 15:45:36 +0200

    Dodanie pól do zagadnienia

--- src/main/java/pl/edu/pw/elka/pik/issueTracker/model/Issue.java
+++ src/main/java/pl/edu/pw/elka/pik/issueTracker/model/Issue.java
@@ -5,6 +5,8 @@ import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
 
+import java.util.Date;
+
 import static javax.persistence.GenerationType.IDENTITY;
 
 /**
@@ -17,24 +19,11 @@ import static javax.persistence.GenerationType.IDENTITY;
 public class Issue {
     private Integer id;
     private String name;
-    public enum TYPE {
-        BUG,
-        ENCHANCEMENT,
-        DUPLICATE,
-        INVALID
-    }
-
-    public TYPE getType() {
-        return type;
-    }
-
-    public void setType(TYPE type) {
-        this.type = type;
-    }
-
-    @Enumerated(EnumType.STRING)
-    @Column(name = "TYPE", nullable = false)
-    private TYPE type;
+    private Type type;
+    private Date created;
+    private Date completed;
+    private Integer priority = 0;
+    private String assignee;
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
@@ -43,16 +32,69 @@ public class Issue {
         return id;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
     @Column(name = "NAME", unique = true, nullable = false, length = 40)
     public String getName() {
         return name;
     }
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "TYPE", nullable = false)
+    public Type getType() {
+        return type;
+    }
+
+    @Column(name = "CREATED", nullable = false)
+    public Date getCreated() {
+        return created;
+    }
+
+    @Column(name = "COMPLETED", nullable = true)
+    public Date getCompleted() {
+        return completed;
+    }
+
+    @Column(name = "PRIORITY", nullable = false)
+    public Integer getPriority() {
+        return priority;
+    }
+
+    @Column(name = "ASSIGNEE", nullable = true, length = 40)
+    public String getAssignee() {
+        return assignee;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public void setCompleted(Date completed) {
+        this.completed = completed;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public void setAssignee(String assignee) {
+        this.assignee = assignee;
+    }
+
+    public enum Type {
+        BUG,
+        ENHANCEMENT,
+        DUPLICATE,
+        INVALID
     }
 }
